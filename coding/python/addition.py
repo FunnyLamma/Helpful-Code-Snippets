7294d3e7719cc0c9483ccacff93510ebd799b571
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2020-12-02 11:00:37 -0800

    Create addition.py

---
+++ coding/python/addition.py
@@ -0,0 +1,7 @@
+#This is a simple addition calculater, I don't know why you would need it but here it is.
+
+def addition():
+    num1 = int(input("What is the first number?"))
+    num2 = int(input("What is the second number?"))
+    ans1 = num1 + num2
+    print(ans1)
