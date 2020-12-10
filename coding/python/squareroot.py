947825809be1d1d971c3af505c2b4eac7feb6fac
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2020-12-10 14:24:07 -0800

    Create squareroot.py

---
+++ coding/python/squareroot.py
@@ -0,0 +1,9 @@
+# This will find the square root of a number #
+
+import math
+
+def exponent2():
+    num1 = int(input("What number?"))
+    ans1 = math.sqrt(num1)
+    print(ans1)
+    time.sleep(3)
