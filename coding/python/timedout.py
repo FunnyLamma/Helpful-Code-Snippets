3316c038076d3a35ff5f7c105433f8d68c296906
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2021-01-04 11:10:05 -0800

    Create timedout.py

---
+++ coding/python/timedout.py
@@ -0,0 +1,9 @@
+This will make it so somthing is timed out if it takes too long.
+
+def timedout():
+    i = 30
+    while i > 0:
+        i = i - 1
+        time.sleep(1)
+        if i == 0:
+            print("Timed Out")
