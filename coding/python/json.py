d052f86188c752d1bc0cede9dd2c21324ceaab97
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2021-01-13 19:23:24 -0800

    Update and rename json.py to readjson.py

--- coding/python/json.py
+++
@@ -1,5 +0,0 @@
-# This is about how to read data from a different file. #
-
-  with open('example.txt') as example:
-    example2 = example.read()
-  print(example2)
---
+++ coding/python/readjson.py
@@ -0,0 +1,5 @@
+# This is about how to read data from a different file. #
+
+  with open('example.txt') as example_file:
+    example_text = example_file.read()
+  print(example_text)
