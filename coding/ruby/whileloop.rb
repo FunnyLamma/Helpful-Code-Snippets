009144a651fd60422594cdfde50c3721266abe58
FunnyLamma <65251658+FunnyLamma@users.noreply.github.com>
2021-02-22 20:56:58 -0800

    Some more stuff.

---
+++ coding/ruby/whileloop.rb
@@ -0,0 +1,7 @@
+## This is a while loop, they are also formatted interestingly, this is a infinite loop though.. ##
+
+example_variable = true
+
+while example_variable == true
+
+    print "This is True!"
